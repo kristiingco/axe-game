8bd8e7c08d8ff50ae978a3583626f0e2020c1ba8
Kristi <kristiana.d.ingco@gmail.com>
2023-04-22 22:58:33 -0700

    Add basic key press functions

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,4 +1,35 @@
 #include "raylib.h"
 int main() {
+    int width = 350;
+    int height = 200;
+
+    InitWindow(width, height, "Axe Game");
+
+    int circle_x = 175;
+    int circle_y = 100;
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        // GAME LOGIC BEGINS 
+
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D)) {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A)) {
+            circle_x = circle_x - 10;
+        }
+
+        // GAME LOGIC ENDS 
+
+
+        EndDrawing();
+    }
+    
 
 }
