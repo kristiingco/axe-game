08fe905153056443eb30f462d68c731c45fef46e
Kristi <kristiana.d.ingco@gmail.com>
2023-04-23 10:10:25 -0700

    Add boundaries to circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -17,11 +17,11 @@ int main() {
 
         DrawCircle(circle_x, circle_y, 25, BLUE);
 
-        if (IsKeyDown(KEY_D)) {
+        if (IsKeyDown(KEY_D) && circle_x < 350) {
             circle_x = circle_x + 10;
         }
 
-        if (IsKeyDown(KEY_A)) {
+        if (IsKeyDown(KEY_A) && circle_x > 0) {
             circle_x = circle_x - 10;
         }
 
