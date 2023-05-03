91f18824efdc321979a44dd28963cbb99048c5ca
Kristi <kristiana.d.ingco@gmail.com>
2023-05-03 11:04:49 -0700

    Add axe

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,13 +1,17 @@
 #include "raylib.h"
 int main() {
-    int width = 350;
-    int height = 200;
+    int width = 800;
+    int height = 450;
 
     InitWindow(width, height, "Axe Game");
 
-    int circle_x = 175;
-    int circle_y = 100;
+    int circle_x = 200;
+    int circle_y = 200;
 
+    int axe_x = 400;
+    int axe_y = 0;
+
+    int direction = 10;
     SetTargetFPS(60);
     while (WindowShouldClose() == false) {
         BeginDrawing();
@@ -16,13 +20,20 @@ int main() {
         // GAME LOGIC BEGINS 
 
         DrawCircle(circle_x, circle_y, 25, BLUE);
+        DrawRectangle(axe_x, axe_y, 50, 50, RED);
 
-        if (IsKeyDown(KEY_D) && circle_x < 350) {
-            circle_x = circle_x + 10;
+        // MOVE AXE
+        axe_y += direction;
+        if(axe_y > height || axe_y < 0) {
+            direction *= -1;
+        }
+
+        if (IsKeyDown(KEY_D) && circle_x < width) {
+            circle_x += 10;
         }
 
         if (IsKeyDown(KEY_A) && circle_x > 0) {
-            circle_x = circle_x - 10;
+            circle_x -= 10;
         }
 
         // GAME LOGIC ENDS 
